3105a7bae29ceaf5f32f3fffb75795a938424634
Heros <sigfridsantamera@hotmail.com>
2018-05-20 21:54:58 +0200

    Agregar estructura de la app

---
+++ js/funcionBotones.js
@@ -0,0 +1,34 @@
+function ponerTarea(id, id2){
+    var tarea = document.getElementById(id).value;
+    var mostrarTarea = document.getElementById(id2);
+    
+    if(tarea.length > 3){ 
+        var parrafo = document.createElement("p");
+        parrafo.innerHTML = tarea;
+        mostrarTarea.appendChild(parrafo);
+    }else{        
+        return false;
+    }
+}
+
+//Permite introducir datos con Enter y vacia el input
+function submitEnter(e){
+    if(e.keyCode == 13){
+        ponerTarea('insertarTexto','mostrarTareas');
+        clearInput('insertarTexto');
+    }
+}
+
+function clearInput(id){
+    document.getElementById(id).value="";
+}
+
+
+
+//Funciona pero no elimina el elemento seleccionado
+
+//~ function eliminar(){
+    //~ let mostrarTarea = document.getElementsByClassName('mostrarTareas')[0];
+    //~ let parrafo = document.getElementsByTagName('p')[0];
+    //~ mostrarTarea.removeChild(parrafo);
+//~ }
