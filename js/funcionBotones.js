f19a6ef7e95b7219e6ab42857b7a8f0dc493d4a7
Heros <sigfridsantamera@hotmail.com>
2018-05-25 23:46:09 +0200

    funcion de borrado completo

--- js/funcionBotones.js
+++ js/funcionBotones.js
@@ -3,14 +3,25 @@ function ponerTarea(id, id2){
     var mostrarTarea = document.getElementById(id2);
     
     if(tarea.length > 3){ 
-        var parrafo = document.createElement("p");
-        parrafo.innerHTML = tarea;
-        parrafo.style.letterSpacing = "1px";
-        mostrarTarea.appendChild(parrafo);
+        var item = document.createElement("div");
+        item.textContent = tarea;
+        item.style.letterSpacing = "1px";
+        item.style.marginTop="15px";
+        item.style.marginBottom = "5px";
+        mostrarTarea.appendChild(item);
+        
+        //eliminamos este elemento al hacer click
+        item.onclick = eliminar;
+        //Al pulsar enter se vacia el contenido
         clearInput('insertarTexto');
     }else{        
         return false;
     }
+}
+
+//Elimina el objeto al hacer click
+function eliminar(id){
+    document.getElementById("mostrarTareas").removeChild(this);
 }
 
 //Permite introducir datos con Enter y vacia el input
